10fe70f0935487f551cb26a4eec78fdc5eefe513
Elijah Insua <tmpvar@gmail.com>
2015-02-08 23:59:28 -0500

    implementation + tests

---
+++ ray-direction-classify.js
@@ -0,0 +1,46 @@
+module.exports = classify;
+
+function classify(i, j, k) {
+  // sign
+  i = (i>0) - (i<0);
+  j = (j>0) - (j<0);
+  k = (k>0) - (k<0);
+
+  // b00110100 === MPO
+  //    ||||||_ k non-zero (false)
+  //    |||||_ k negative (false)
+  //    ||||_ j non-zero (true)
+  //    |||_ j negative (false)
+  //    ||_ i non-zero (true)
+  //    |_ i negative (true)
+  return (i>>>-1) << 5 | ((i>>>0)&1) << 4 |
+         (j>>>-1) << 3 | ((j>>>0)&1) << 2 |
+         (k>>>-1) << 1 | (k>>>0)&1;
+}
+
+classify.MMM = 63; // b00111111
+classify.MMP = 61; // b00111101
+classify.MPM = 55; // b00110111
+classify.MPP = 53; // b00110101
+classify.PMM = 31; // b00011111
+classify.PMP = 29; // b00011101
+classify.PPM = 23; // b00010111
+classify.PPP = 21; // b00010101
+classify.POO = 16; // b00010000
+classify.MOO = 48; // b00110000
+classify.OPO =  4; // b00000100
+classify.OMO = 12; // b00001100
+classify.OOP =  1; // b00000001
+classify.OOM =  3; // b00000011
+classify.OMM = 15; // b00001111
+classify.OMP = 13; // b00001101
+classify.OPM =  7; // b00000111
+classify.OPP =  5; // b00000101
+classify.MOM = 51; // b00110011
+classify.MOP = 49; // b00110001
+classify.POM = 19; // b00010011
+classify.POP = 17; // b00010001
+classify.MMO = 60; // b00111100
+classify.MPO = 52; // b00110100
+classify.PMO = 28; // b00011100
+classify.PPO = 20; // b00010100
---
+++ test.js
@@ -0,0 +1,110 @@
+var test = require('tape');
+var classify = require('./ray-direction-classify');
+console.log(classify)
+
+test('the different combinations (int)', function(t) {
+  [
+    [-1, -1, -1, classify.MMM],
+    [-1, -1,  1, classify.MMP],
+    [-1,  1, -1, classify.MPM],
+    [-1,  1,  1, classify.MPP],
+    [ 1, -1, -1, classify.PMM],
+    [ 1, -1,  1, classify.PMP],
+    [ 1,  1, -1, classify.PPM],
+    [ 1,  1,  1, classify.PPP],
+    [ 1,  0,  0, classify.POO],
+    [-1,  0,  0, classify.MOO],
+    [ 0,  1,  0, classify.OPO],
+    [ 0, -1,  0, classify.OMO],
+    [ 0,  0,  1, classify.OOP],
+    [ 0,  0, -1, classify.OOM],
+    [ 0, -1, -1, classify.OMM],
+    [ 0, -1,  1, classify.OMP],
+    [ 0,  1, -1, classify.OPM],
+    [ 0,  1,  1, classify.OPP],
+    [-1,  0, -1, classify.MOM],
+    [-1,  0,  1, classify.MOP],
+    [ 1,  0, -1, classify.POM],
+    [ 1,  0,  1, classify.POP],
+    [-1, -1,  0, classify.MMO],
+    [-1,  1,  0, classify.MPO],
+    [ 1, -1,  0, classify.PMO],
+    [ 1,  1,  0, classify.PPO]
+  ].forEach(function(a) {
+    t.equal(classify(a[0], a[1], a[2]), a[3]);
+  });
+
+  t.end();
+});
+
+test('the different combinations (int)', function(t) {
+  var e = [
+    [-1, -1, -1, classify.MMM],
+    [-1, -1,  1, classify.MMP],
+    [-1,  1, -1, classify.MPM],
+    [-1,  1,  1, classify.MPP],
+    [ 1, -1, -1, classify.PMM],
+    [ 1, -1,  1, classify.PMP],
+    [ 1,  1, -1, classify.PPM],
+    [ 1,  1,  1, classify.PPP],
+    [ 1,  0,  0, classify.POO],
+    [-1,  0,  0, classify.MOO],
+    [ 0,  1,  0, classify.OPO],
+    [ 0, -1,  0, classify.OMO],
+    [ 0,  0,  1, classify.OOP],
+    [ 0,  0, -1, classify.OOM],
+    [ 0, -1, -1, classify.OMM],
+    [ 0, -1,  1, classify.OMP],
+    [ 0,  1, -1, classify.OPM],
+    [ 0,  1,  1, classify.OPP],
+    [-1,  0, -1, classify.MOM],
+    [-1,  0,  1, classify.MOP],
+    [ 1,  0, -1, classify.POM],
+    [ 1,  0,  1, classify.POP],
+    [-1, -1,  0, classify.MMO],
+    [-1,  1,  0, classify.MPO],
+    [ 1, -1,  0, classify.PMO],
+    [ 1,  1,  0, classify.PPO]
+  ]
+
+  e.forEach(function(a, i) {
+    t.notEqual(classify(a[0], a[1], a[2]), e[(i+1)%e.length][3]);
+  });
+
+  t.end();
+});
+
+test('the different combinations (int)', function(t) {
+  [
+    [-0.1, -0.1, -0.1, classify.MMM],
+    [-0.1, -0.1,  0.1, classify.MMP],
+    [-0.1,  0.1, -0.1, classify.MPM],
+    [-0.1,  0.1,  0.1, classify.MPP],
+    [ 0.1, -0.1, -0.1, classify.PMM],
+    [ 0.1, -0.1,  0.1, classify.PMP],
+    [ 0.1,  0.1, -0.1, classify.PPM],
+    [ 0.1,  0.1,  0.1, classify.PPP],
+    [ 0.1,  0.0,  0.0, classify.POO],
+    [-0.1,  0.0,  0.0, classify.MOO],
+    [ 0.0,  0.1,  0.0, classify.OPO],
+    [ 0.0, -0.1,  0.0, classify.OMO],
+    [ 0.0,  0.0,  0.1, classify.OOP],
+    [ 0.0,  0.0, -0.1, classify.OOM],
+    [ 0.0, -0.1, -0.1, classify.OMM],
+    [ 0.0, -0.1,  0.1, classify.OMP],
+    [ 0.0,  0.1, -0.1, classify.OPM],
+    [ 0.0,  0.1,  0.1, classify.OPP],
+    [-0.1,  0.0, -0.1, classify.MOM],
+    [-0.1,  0.0,  0.1, classify.MOP],
+    [ 0.1,  0.0, -0.1, classify.POM],
+    [ 0.1,  0.0,  0.1, classify.POP],
+    [-0.1, -0.1,  0.0, classify.MMO],
+    [-0.1,  0.1,  0.0, classify.MPO],
+    [ 0.1, -0.1,  0.0, classify.PMO],
+    [ 0.1,  0.1,  0.0, classify.PPO]
+  ].forEach(function(a) {
+    t.equal(classify(a[0], a[1], a[2]), a[3]);
+  });
+
+  t.end();
+});
